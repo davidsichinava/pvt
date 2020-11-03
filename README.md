0c3cae07a375bd5d63a0edbaa5d14f4eccfd4d00
David Sichinava <david@sichinava.ge>
2020-11-03 10:29:18 -0500

    add chart and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+### Parallel vote tabulation results for elections in the Republic of Georgia, 2003-2020
+
+* PVT.xlsx - raw data sourced from civil.ge, the CEC website, Wikipedia (election results for 2003-2004), and the website of the International Society For Free Elections and Democracy (ISFED).
+* analysis.R - generates the chart below:
+
+![Chart](pvt_cec_results.png)
+
+This dataset contains available data on parallel vote tabulation conducted by the International Society For Free Elections and Democracy (ISFED) in Georgia. In several cases data on error margins was not included in the analyzed sources.
--- analysis.R
+++ analysis.R
@@ -8,7 +8,8 @@ mean(pvt$Modulo)
 
 pvt %>%
   filter(Modulo > 1)%>%
-  data.table::melt(value.name = "value", id.vars=c("Elections", "Cand", "Modulo"), measure.vars=c("ISFED", "CEC"))%>%
+  mutate(diff = CEC-ISFED)%>%
+  data.table::melt(value.name = "value", id.vars=c("Elections", "Cand", "Modulo", "diff"), measure.vars=c("ISFED", "CEC"))%>%
   mutate(who = paste0(Cand, ", ", Elections))%>%
   ggplot(aes(reorder(who, Modulo), value, group=variable, label=value, fill = variable))+
   geom_col(position=position_dodge())+
@@ -17,13 +18,11 @@ pvt %>%
   coord_flip()+
   ylim(0, 70)+
   theme_minimal()+
-  annotate("text", x=4, y = 65,  label = paste0("Diff: +", pvt$Modulo[pvt$Elections=="2008 Pres" & pvt$Cand=="Saakashvili"]), fontface="bold", color = "#60c028")+
-  annotate("text", x=3, y = 15,  label = paste0("Diff: -", pvt$Modulo[pvt$Elections=="2004 Parl" & pvt$Cand=="Aghordzineba"]), fontface="bold", color = "#af1615")+
-  annotate("text", x=2, y = 60,  label = paste0("Diff: +", pvt$Modulo[pvt$Elections=="2020 Parliamentary" & pvt$Cand=="GD"]), fontface="bold", color = "#60c028")+
-  annotate("text", x=1, y = 50,  label = paste0("Diff: +", pvt$Modulo[pvt$Elections=="2018 1" & pvt$Cand=="Zourabichvili"]), fontface="bold", color = "#60c028")+
   theme(legend.position = "bottom",
         legend.title = element_blank(),
         axis.title = element_blank())+
-  labs(title="Parallel Vote Tabulation vs. Official CEC results",
-       subtitle = "2004-2020",
+  labs(title="Parallel Vote Tabulation vs. Official CEC results, 2004-2020",
+       subtitle = "Only differences more than 1% are displayed.\nSome statistically significant differences tally to less than 1%",
        caption = "Source: ISFED website, CEC website, Civil.ge")
+
+ggsave("pvt_cec_results.png", width = 10, height=8)
